99074d98194fd4ccc8c046d724abee05c4b32745
Daria <daria.kiseleva@mail.mcgill.ca>
2022-06-13 13:41:46 -0400

    Add simple routing

--- drafts/daria-auth-test/src/App.js
+++ drafts/daria-auth-test/src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import Login from "./components/Login"
+import Self from "./components/Self"
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Some app</h1>
 
+      <Router>
+        <Routes>
 
-    
+          <Route path="/" element={<Login />}/>
+
+          <Route path="/self" element={<Self />}/>
+
+        </Routes>
+      </Router>
 
 
     </div>
---
+++ drafts/daria-auth-test/src/components/Self.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Self() {
+  return (
+    <h1>Self</h1>
+  )
+}
+
+export default Self
