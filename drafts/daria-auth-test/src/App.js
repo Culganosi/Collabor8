3b82b06cdf8d39eb202a7417f94e994739817389
Daria <daria.kiseleva@mail.mcgill.ca>
2022-06-13 13:15:30 -0400

    Create drafts directory and make a test client

---
+++ drafts/daria-auth-test/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Some app</h1>
+
+
+    
+
+
+    </div>
+  );
+}
+
+export default App;
