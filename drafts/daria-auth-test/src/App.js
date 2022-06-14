058643baa716552e441d9cfe4241deb3c9643dfd
Daria <daria.kiseleva@mail.mcgill.ca>
2022-06-14 03:58:15 -0400

    Get id of partners into chat previews for each user

--- drafts/daria-auth-test/src/App.js
+++ drafts/daria-auth-test/src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import React, {useState, useEffect} from "react";
 
 import Login from "./components/Login"
-import Chat from "./components/Chat"
+import Chat from "./components/Chat/"
 
 import {DataContext} from "./DataContext"
 
@@ -14,29 +14,48 @@ import {DataContext} from "./DataContext"
 function App() {
 
     //This info is obtained upon successful login or registration
-    const [self, setSelf] = useState({empty: "yes"});
+    const [self, setSelf] = useState({empty: "no self / not logged in"});
+    const [chatPreviews, setChatPreviews] = useState([])
 
   // --------- Local 
 
+  //Every time the app loads
   useEffect(() => {
+
+    //Find out who the client is based on their cookie
     axios.get("/users/self")
     .then(res => {
-
-
       const clientInfo = res.data;
       setSelf(res.data)
+      return clientInfo._id;
+    })
+    .then(clientId => {
 
+      console.log(clientId)
 
+      //TODO: Have rooms for all conversations of the client
+      //Also what if messages from someone not messaged before come in? Should also be a room
 
 
     })
     .catch(error => console.log(error.message))
+
+
+
+    
+
+
+
+
   }, [])
 
 
   return (
 
-    <DataContext.Provider value={{self, setSelf}}>
+    <DataContext.Provider value={{
+       self, setSelf,
+       chatPreviews, setChatPreviews
+    }}>
 
       <Router>
 
---
+++ drafts/daria-auth-test/src/components/Chat/Conversation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./chatStyle.css"
+
+
+function Conversation() {
+  return (
+    <div className="chat__conversation">Conversation</div>
+  )
+}
+
+export default Conversation
