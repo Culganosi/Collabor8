876b67d2bef08d009ff89b0985c29b2ae7808dd1
Daria <daria.kiseleva@mail.mcgill.ca>
2022-07-17 19:29:14 -0400

    Debug logo animation alignment (kinda)

--- client/src/components/LogoAnimation.jsx
+++ client/src/components/LogoAnimation.jsx
@@ -1,27 +1,6 @@
 import React from 'react'
-// import CollabLogoIntro from "../media/CollabLogoIntro.mp4";
 import IntroCollabPurple from "../media/IntroCollabPurple.mp4"
-// import { Player } from 'video-react';
-// import CollabLogoIntro from "../media/CollabLogoIntro.mp4";
 import "./LogoAnimation.css"
-// import "./../../node_modules/video-react/dist/video-react.css"; // import css
-
-// import '~video-react/dist/video-react.css'; // import css
-{/* <link rel="stylesheet" href="/css/video-react.css" /> */}
-// Has to be muted even if no sound autoplay true muted true 
-//position:100, muted: true 
-
-// export default function LogoAnimation() {
-  
-//   return (
-// <Player className="logo"
-// playsInline
-// src={CollabLogoIntro}
-// // position={100}
-// muted={true}
-//  />
-//     )
-// }
 
 
 export default function LogoAnimation() {    
