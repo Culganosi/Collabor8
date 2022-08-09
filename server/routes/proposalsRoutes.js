345c940aef85b91e4f1fb83cfba3361c90169e96
Daria <daria.kiseleva@mail.mcgill.ca>
2022-08-09 03:37:19 -0400

    Create route that returns active proposals by author ID

--- server/routes/proposalsRoutes.js
+++ server/routes/proposalsRoutes.js
@@ -2,171 +2,193 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (User, Proposal) => {
+  //Get minimal info about proposals for the "browse proposals" page
+  //See GET /users for comments because it's the same concepts
+  router.get("/", async (req, res) => {
+    console.log("In GET /proposals");
 
+    //User must be logged in
+    if (!req.session.userId) {
+      return res.status(403).json({ message: "You're not logged in" });
+    }
+    const selfId = req.session.userId;
 
-     
+    let { filterInput, sortInput } = req.body;
 
-    //Get minimal info about proposals for the "browse proposals" page
-    //See GET /users for comments because it's the same concepts
-     router.get("/", async (req, res) => {
-          console.log("In GET /proposals")
+    if (!filterInput) {
+      filterInput = {};
+    }
+    if (filterInput?.title) {
+      filterInput.title = { $regex: filterInput.title, $options: "i" };
+    }
 
-          //User must be logged in
-          if (!req.session.userId) {
-               return res.status(403).json({message: "You're not logged in"})
-          }
-          const selfId = req.session.userId;
-          
-          let {filterInput, sortInput} = req.body;
+    //TODO: This is not right, it should return proposals that seek ANY of the skills listed, not ALL of them
+    //Figure out how to do this but keep this for now
+    //https://stackoverflow.com/questions/19648841/how-can-i-handle-array-intersection-in-find
+    if (filterInput.seeking) {
+      filterInput.seeking = { $all: filterInput.seeking };
+    }
+    //Filter only for active proposals
+    if (filterInput) {
+      filterInput.status = "Active";
+    }
 
-          if (!filterInput) {
-               filterInput={}
-          }
-          if (filterInput?.title) {
-               filterInput.title = { "$regex": filterInput.title, "$options": "i" }
-          }
-          
-          //TODO: This is not right, it should return proposals that seek ANY of the skills listed, not ALL of them
-          //Figure out how to do this but keep this for now
-          //https://stackoverflow.com/questions/19648841/how-can-i-handle-array-intersection-in-find
-          if  (filterInput.seeking) {
-               filterInput.seeking = {$all : filterInput.seeking}
-          }
-          //Filter only for active proposals
-          if(filterInput) {
-               filterInput.status = "Active";
-          }
+    //Exclude proposals by the user who is logged in
+    filterInput.author = { $ne: selfId };
 
-           //Exclude proposals by the user who is logged in
-           filterInput.author = {"$ne": selfId}
+    const fieldsToReturn = {
+      title: 1,
+      author: 1,
+      createdAt: 1,
+      seeking: 1,
+      shortDescription: 1,
+      image: 1,
+    };
 
-          const fieldsToReturn = {title: 1, author: 1, createdAt: 1, seeking: 1, shortDescription: 1, image: 1}
+    Proposal.find(filterInput, fieldsToReturn)
+      .sort("-createdAt")
+      .then((proposalData) => {
+        //Make the data an object indexable by ID
+        const rearrangedData = {};
+        proposalData.forEach((proposal) => {
+          rearrangedData[proposal._id] = proposal;
+        });
 
-          Proposal
-          .find(filterInput, fieldsToReturn) 
-          .sort("-createdAt")
-          .then((proposalData) => {
+        res.json(rearrangedData);
+      })
+      .catch((dbError) => res.status(500).json({ error: dbError.message }));
+  });
 
-               //Make the data an object indexable by ID
-               const rearrangedData = {};
-               proposalData.forEach(proposal => {
-                    rearrangedData[proposal._id] = proposal;
-               })
+  //Return all the proposals of the Self user, including inactive ones
+  router.get("/self", async (req, res) => {
+    console.log("In GET /proposals/self");
 
-               res.json(rearrangedData)
-          })
-          .catch(dbError => res.status(500).json({error: dbError.message}))
+    if (!req.session.userId) {
+      return res.status(403).json({ message: "You're not logged in" });
+    }
 
+    const userId = req.session.userId;
+    const selfProposals = await Proposal.find(
+      { author: userId },
+      { description: 0 }
+    );
+    return res.status(200).json(selfProposals);
+  });
 
-    })
+  //Return only active proposals of a user by userID
+  router.get("/author/:authorId", async (req, res) => {
+    console.log("In GET /proposals/author/:authorId");
 
-     //Return all the proposals of the Self user, including inactive ones
-     router.get('/self', async (req, res) => {
-          console.log("In GET /proposals/self")
+    //Find a list of IDs of active proposals of the author
+    const authorId = req.params.authorId;
+    const author = await User.findById(authorId);
+    const activePropIds = author.activeProposals;
 
-          if(!req.session.userId) {
-               return res.status(403).json({message: "You're not logged in"})
-          }
-          
-          const userId = req.session.userId;
-          const selfProposals = await Proposal.find({author: userId}, {description: 0})
-          return res.status(200).json(selfProposals)
-     })
+    //Extract necessary details of author's active proposals
+    const activeProps = await Proposal.find(
+      { _id: { $in: activePropIds } },
+      { description: 0, status: 0 }
+    ).sort("-createdAt");
 
-    //View a specific proposal on its own page
-    router.get("/:proposalId", async (req, res) => {
-          console.log("In GET /proposals/:id")
+    //Return the list of proposals
+    res.json(activeProps);
+  });
 
-          const targetProposal = await Proposal.findById(req.params.proposalId, {"__v": 0})
-          res.json(targetProposal)
-    })
+  //View a specific proposal on its own page
+  router.get("/:proposalId", async (req, res) => {
+    console.log("In GET /proposals/:id");
 
-    //Create a new proposal and add it to the list of the authors either active or inactive proposals
-    router.post("/", async (req, res) => {
-          console.log("In POST /proposals")
-          if(!req.session.userId) {
-               return res.status(403).json({message: "You're not logged in"})
-          }
+    const targetProposal = await Proposal.findById(req.params.proposalId, {
+      __v: 0,
+    });
+    res.json(targetProposal);
+  });
 
-          const inputInfo = req.body
+  //Create a new proposal and add it to the list of the authors either active or inactive proposals
+  router.post("/", async (req, res) => {
+    console.log("In POST /proposals");
+    if (!req.session.userId) {
+      return res.status(403).json({ message: "You're not logged in" });
+    }
 
-          //We get the author from cookie info
-          const authorId = req.session.userId;
+    const inputInfo = req.body;
 
-          //If no image added, add a default gradient
-          //TODO: Storing images on our own server?
-          if (!inputInfo?.image){
-               inputInfo.image = "https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2020/02/Usign-Gradients-Featured-Image.jpg"
-          }
+    //We get the author from cookie info
+    const authorId = req.session.userId;
 
-          const author = await User.findById(authorId)
-          if (!author) {
-               return res.status(404).json({message: "Invalid author ID"})
-          }
+    //If no image added, add a default gradient
+    if (!inputInfo?.image) {
+      inputInfo.image =
+        "https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2020/02/Usign-Gradients-Featured-Image.jpg";
+    }
 
-          const newProposal = new Proposal({...inputInfo, createdAt: Date.now(), author: authorId})
-          insertedProposal = await newProposal.save()
+    const author = await User.findById(authorId);
+    if (!author) {
+      return res.status(404).json({ message: "Invalid author ID" });
+    }
 
-          if (!insertedProposal) {
-               return res.status(500).json({message: error.message})
-          }
+    const newProposal = new Proposal({
+      ...inputInfo,
+      createdAt: Date.now(),
+      author: authorId,
+    });
+    insertedProposal = await newProposal.save();
 
-          if (inputInfo.status === "Active") {    
-               author.activeProposals.push(insertedProposal._id)
-          } else {
-               author.inactiveProposals.push(insertedProposal._id)
-          }      
+    if (!insertedProposal) {
+      return res.status(500).json({ message: error.message });
+    }
 
-          await author.save();
+    if (inputInfo.status === "Active") {
+      author.activeProposals.push(insertedProposal._id);
+    } else {
+      author.inactiveProposals.push(insertedProposal._id);
+    }
 
-          return res.status(201).json({message: "successs", proposal: insertedProposal})
+    await author.save();
 
-    })
+    return res
+      .status(201)
+      .json({ message: "successs", proposal: insertedProposal });
+  });
 
-    //Edit proposal (change to active/inactive, or change content details)
-    //See Patch route for users for comments
-    router.patch("/:proposalId", async (req, res) => {
+  //Edit proposal (change to active/inactive, or change content details)
+  //See Patch route for users for comments
+  router.patch("/:proposalId", async (req, res) => {
+    console.log("In PATCH /proposals/:id");
 
-          console.log("In PATCH /proposals/:id")
+    const inputFields = req.body;
+    Proposal.updateOne({ _id: req.params.proposalId }, inputFields)
+      .then(() => res.status(200).json({ message: "success" }))
+      .catch((error) => res.status(400).json({ message: error.message }));
+  });
 
-          const inputFields = req.body;
-          Proposal.updateOne({"_id": req.params.proposalId}, inputFields)
-          .then(() => res.status(200).json({message: "success"}))
-          .catch((error) => res.status(400).json({message: error.message}))
-    })
+  //Permanently delete a proposal
+  router.delete("/:proposalId", async (req, res) => {
+    console.log("In DELETE /proposals/:id");
 
-    //Permanently delete a proposal
-    router.delete("/:proposalId", async (req, res) => {
+    const targetProposalId = req.params.proposalId;
 
-          console.log("In DELETE /proposals/:id")
+    const targetProposal = await Proposal.findById(targetProposalId);
+    const proposalAuthorId = targetProposal.author;
 
-          const targetProposalId = req.params.proposalId
+    const targetAuthor = await User.findById(proposalAuthorId);
 
-          const targetProposal = await Proposal.findById(targetProposalId)
-          const proposalAuthorId = targetProposal.author
+    if (targetAuthor.activeProposals.includes(targetProposalId)) {
+      const index = targetAuthor.activeProposals.indexOf(targetProposalId);
+      targetAuthor.activeProposals.splice(index, 1);
+      await targetAuthor.save();
+    }
 
-          const targetAuthor = await User.findById(proposalAuthorId)
-          
-          if (targetAuthor.activeProposals.includes(targetProposalId)) {
-               const index = targetAuthor.activeProposals.indexOf(targetProposalId);
-               targetAuthor.activeProposals.splice(index, 1)
-               await targetAuthor.save()
-          } 
+    if (targetAuthor.inactiveProposals.includes(targetProposalId)) {
+      const index = targetAuthor.inactiveProposals.indexOf(targetProposalId);
+      targetAuthor.inactiveProposals.splice(index, 1);
+      await targetAuthor.save();
+    }
 
-          if (targetAuthor.inactiveProposals.includes(targetProposalId)) {
-               const index = targetAuthor.inactiveProposals.indexOf(targetProposalId);
-               targetAuthor.inactiveProposals.splice(index, 1)
-               await targetAuthor.save()
-          } 
+    Proposal.findByIdAndRemove(targetProposalId)
+      .then(() => res.status(200).json({ message: "success" }))
+      .catch((error) => res.status(400).json({ message: error.message }));
+  });
 
-         Proposal.findByIdAndRemove(targetProposalId)
-         .then(() => res.status(200).json({message: "success"}))
-         .catch((error) => res.status(400).json({message: error.message}))
-    })
-
-
-
-
-    return router;
-
+  return router;
 };
