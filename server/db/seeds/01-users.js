17ed1e41f15bb2572de4de0f133fa868ac17b92a
Daria <daria.kiseleva@mail.mcgill.ca>
2022-08-22 09:38:56 -0400

    Add demo user to database seed

--- server/db/seeds/01-users.js
+++ server/db/seeds/01-users.js
@@ -1,228 +1,199 @@
-
 const seedUsers = [
   {
-      "userhandle": "Sparkling-Alligator",
-      "password": "123",
-      "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.n9m2a8vsjh?alt=media&token=975ee5dc-42cf-4a5b-83a8-c3fdbc5bb6d4",
-      "bio": "I'm an aspiring UX/UI designer and a recent gradate from a UX/UI bootcamp, with previous experience as a graphic designer. I love designing digital products that are beautiful, accessible, and intuitive to use. My particular interest right now are social media apps!\n\nI'm looking to practice my skills more and to collab with any kind of web developers. I have some prototypes I would love to have built into web apps, but I'm also open for working on the ideas of others. Message me to chat about potential projects!",
-      "role": "UX/UI designer",
-      "shortBio": "Early-career designer on the prowl for collabs!",
-      "socialMedia": {
-          "Portfolio": "https://www.etsy.com/market/alligator_portfolio",
-          "LinkedIn": "https://www.linkedin.com/"
-        },
-        "skills": [
-          "Photoshop",
-          "Figma",
-          "UX research",
-          "HTML",
-          "CSS"
-        ],
+    userhandle: "DemoUser",
+    password: "123",
+    shortBio: "I am a demo user!",
+    bio: "Just an account from which anyone can test this app.",
+    avatar:
+      "https://www.xmple.com/wallpaper/black-pink-gradient-linear-1920x1080-c2-000000-ff1493-a-60-f-14.svg",
+    role: "Back-end developer",
+    socialMedia: {
+      Portfolio: "https://github.io",
+      LinkedIn: "https://www.linkedin.com/",
+    },
+    skills: ["SQL", "Express", "NodeJS", "MongoDB", "HTML", "CSS", "Git"],
   },
   {
-      userhandle: "SQLrillex",
-      "password": "123",
-      "skills": [
-          "SQL",
-          "Express",
-          "NodeJS",
-          "MongoDB",
-          "HTML",
-          "CSS",
-          "Git",
-          "TDD",
-          "VSCode"
-      ],
-      "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.u8lh2s9e1?alt=media&token=2f1065f2-4027-4a57-9515-030e422280a3",
-      "bio": "I'm a back-end engineer specializing in database design. I'm interested in building fun passion project and getting to know more developers and designers. In particular, I'd love to find some front-end and UX/UI collaborators who can design and build appealing and user-friendly interfaces for my apps.",
-      "role": "Back-end developer",
-      "shortBio": "Database design is my passion",
-      "socialMedia": {}
+    userhandle: "Sparkling-Alligator",
+    password: "123",
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.n9m2a8vsjh?alt=media&token=975ee5dc-42cf-4a5b-83a8-c3fdbc5bb6d4",
+    bio: "I'm an aspiring UX/UI designer and a recent gradate from a UX/UI bootcamp, with previous experience as a graphic designer. I love designing digital products that are beautiful, accessible, and intuitive to use. My particular interest right now are social media apps!\n\nI'm looking to practice my skills more and to collab with any kind of web developers. I have some prototypes I would love to have built into web apps, but I'm also open for working on the ideas of others. Message me to chat about potential projects!",
+    role: "UX/UI designer",
+    shortBio: "Early-career designer on the prowl for collabs!",
+    socialMedia: {
+      Portfolio: "https://www.etsy.com/market/alligator_portfolio",
+      LinkedIn: "https://www.linkedin.com/",
+    },
+    skills: ["Photoshop", "Figma", "UX research", "HTML", "CSS"],
   },
   {
-      "userhandle": "full_stack_pancake",
-      "password": "123",
-      "skills": [
-        "HTML",
-        "CSS",
-        "Sass",
-        "ReactJS",
-        "Express",
-        "NodeJS",
-        "SQL",
-        "MongoDB",
-        "Git",
-        "TDD",
-        "VSCode",
-        "AngularJS"
-      ],
-      "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.zy6l4c021x?alt=media&token=ec7cf87c-d479-4c9e-892a-d14f9ff6e418",
-      "bio": "I'm a self-taught web developer with one year of work experience. I seek interesting and challenging projects to practice new technologies and add to my personal portfolio. \n\nI'm looking to collaborate with and learn from all kinds of developers and designers, and I'm particularly interested in building larger projects that are planned thoughtfully and are meant to scale. \n\nMessage me to discuss any ideas or just to chat!",
-      "role": "Full-stack developer",
-      "shortBio": "Passionate about mastering web development across the stack. Tech addict. Syrup enthusiast. ",
-      "socialMedia": {
-        "Portfolio": "https://www.stackedpancakehouse.ca/breakfast.php",
-        "Twitter": "https://twitter.com/pancakeswap"
-      }
-  },
-  {
-    "userhandle": "troybarnes",
-    "password": "123",
-    "skills": [
-      "HTML",
-      "CSS",
-      "Sass",
-      "Photoshop"
-    ],
-    "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.ae1qvbghge?alt=media&token=e4e4c32d-20a1-4c9e-8d4c-8674c112b11a",
-    "bio": "I have an interest in making websites that have a more cinematic feel to it. I have experience in film as well, which greatly helps. Also check out my podcast: Troy and Abed in the Morning",
-    "role": "UX/UI designer",
-    "shortBio": "I have a background in UI/UX design & would love to offer my services to others.",
-    "socialMedia": {
-      "Portfolio": "https://www.stackedpancakehouse.ca/breakfast.php",
-      "GitHub": "http://google.com",
-      "Twitter": "https://twitter.com/pancakeswap",
-
-    }
-  },
-  {
-    "userhandle": "Chewbacca",
-    "password": "123",
-    "skills": [
-      "HTML",
-      "CSS",
-      "Sass",
-      "ReactJS"
-    ],
-    "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.lusjl827ek?alt=media&token=564a692c-c25e-4b9f-823f-5e5ae7383110",
-    "bio": "RRRAARRWHHGWWR. RRRAARRWHHGWWR. RRRAARRWHHGWWR. RRRAARRWHHGWWR.",
-    "role": "Front-end developer",
-    "shortBio": "RRWWWGG. RRRAARRWHHGWWR. RRRAARRWHHGWWR. ",
-    "socialMedia": {
-      "GitHub": "http://google.com"
-    }
-  },
-  {
-    "userhandle": "sixseasons",
-    "password": "123",
-    "skills": [
-      "NodeJS",
-      "Express",
-      "ReactJS",
-      "AngularJS",
-      "Git"
-    ],
-    "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.w9gv0aqvft?alt=media&token=aa5ae677-4f64-41fe-ba67-473fad6f74d2",
-    "bio": "I just want to work on other projects for fun. I already work full-time so only available for smaller projects.",
-    "role": "Full-stack developer",
-    "shortBio": "Love all things tech and very experienced in terms of the user experience and the back-end dev't",
-    "socialMedia": {
-      "Portfolio": "http://google.com",
-      "GitHub": "http://google.com",
-      "LinkedIn": "http://google.com",
-      "Twitter": "http://google.com"
-    }
-  },
-  {
-    "userhandle": "Moe Szyslak",
-    "password": "123",
-    "skills": [
-      "Figma",
-      "Express",
-      "NodeJS",
-      "HTML",
-      "CSS",
-      "ReactJS",
-      "Git"
-    ],
-    "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.hsoktdkmcy?alt=media&token=82281a09-c59e-4351-9e93-5811b265a9be",
-    "bio": "Moe, Moe, Moe! How do you like me? How do you like me? Moe, Moe, Moe! Why don't you like me? Nobody likes me...",
-    "role": "Full-stack developer",
-    "shortBio": "I own a bar. People come to my bar. That's it.",
-    "socialMedia": {
-      "GitHub": "http://google.com"
-    }
-  },
-  {
-    "userhandle": "baconandeggs",
-    "password": "123",
-    "skills": [
-      "Git",
-      "NodeJS",
-      "Express",
-      "HTML",
-      "CSS",
-      "MongoDB",
-      "SQL"
-    ],
-    "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.9133alksly?alt=media&token=302d7039-8a42-4c61-90bb-8c9a090324a0",
-    "bio": "Need to work with people who have a better eye for improving the user experience and have more experience with that kind of work.",
-    "role": "Back-end developer",
-    "shortBio": "Previously worked on corporate websites and would love to work with someone more creative",
-    "socialMedia": {
-      "Portfolio": "http://google.com",
-      "GitHub": "http://google.com"
-    }
-  },
-  {
-    "userhandle": "Master Chief",
-    "password": "123",
-    "skills": [
-      "HTML",
-      "CSS",
-      "Figma",
-      "Photoshop",
-      "UX research",
-      "Git",
-      
-    ],
-    "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.9m88v7kq4o?alt=media&token=0404be94-b805-4155-971e-8a076c8da465",
-    "bio": "\"I do not remember my name. I do not remember my family. I do not remember my home. But I can remember the game. We played it every day. And I never lost. The game... It's the only thing I can remember about the life I had before I met Doctor Halsey. Since then, I have experienced a lifetime of combat. Through thirty years of war against alien aggressors... I have always known my fate. I knew someday I would die in battle. But now that it is here... now that it is time to die... I find I am not ready.\"",
-    "role": "UX/UI designer",
-    "shortBio": "A towering supersoldier known as a \"Spartan\", raised and trained from childhood for combat.",
-    "socialMedia": {
-      "Twitter": "http://google.com"
-    }
-  },
-  {
-    "userhandle": "NedQueens",
-    "password": "123",
-    "skills": [
-      "ReactJS",
-      "NodeJS",
-      "MongoDB",
-      "Express",
-      "Git",
-    ],
-    "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.hzkbzunfw4?alt=media&token=f7fdd3a0-8bcd-4024-b470-89627774618a",
-    "bio": "I have some experience and am very tech-savy. I have worked on a couple projects myself but am not allowed to talk about it.",
-    "role": "Full-stack developer",
-    "shortBio": "I'm just a guy from Queens who needs to practice more. So please DM me",
-    "socialMedia": {
-      "Twitter": "http://google.com",
-      "GitHub": "http://google.com"
-    }
-  },
-  {
-    "userhandle": "dwightschrute",
-    "password": "123",
-    "skills": [
-      "HTML",
-      "CSS",
-      "NodeJS",
-      "Express",
+    userhandle: "SQLrillex",
+    password: "123",
+    skills: [
       "SQL",
+      "Express",
+      "NodeJS",
+      "MongoDB",
+      "HTML",
+      "CSS",
+      "Git",
+      "TDD",
+      "VSCode",
+    ],
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.u8lh2s9e1?alt=media&token=2f1065f2-4027-4a57-9515-030e422280a3",
+    bio: "I'm a back-end engineer specializing in database design. I'm interested in building fun passion project and getting to know more developers and designers. In particular, I'd love to find some front-end and UX/UI collaborators who can design and build appealing and user-friendly interfaces for my apps.",
+    role: "Back-end developer",
+    shortBio: "Database design is my passion",
+    socialMedia: {},
+  },
+  {
+    userhandle: "full_stack_pancake",
+    password: "123",
+    skills: [
+      "HTML",
+      "CSS",
+      "Sass",
+      "ReactJS",
+      "Express",
+      "NodeJS",
+      "SQL",
+      "MongoDB",
+      "Git",
+      "TDD",
+      "VSCode",
       "AngularJS",
     ],
-    "avatar": "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.5f13pa4aej?alt=media&token=078dbb61-93fd-4fda-9013-fe889f99bdd7",
-    "bio": "I have experience in the food industry which is why my interest expands into healthy eating on top of tech. I am a recent bootcamp graduate, I am practicing my skills and learning new technologies across the stack. I can sell anything, I just need someone to help me with the visual aspects of my proposals.",
-    "role": "Full-stack developer",
-    "shortBio": "I am pretty new to web development and have a background in the corporate industry. ",
-    "socialMedia": {
-      "LinkedIn": "http://google.com",
-      "GitHub": "http://google.com"
-    }
-  }
-]
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.zy6l4c021x?alt=media&token=ec7cf87c-d479-4c9e-892a-d14f9ff6e418",
+    bio: "I'm a self-taught web developer with one year of work experience. I seek interesting and challenging projects to practice new technologies and add to my personal portfolio. \n\nI'm looking to collaborate with and learn from all kinds of developers and designers, and I'm particularly interested in building larger projects that are planned thoughtfully and are meant to scale. \n\nMessage me to discuss any ideas or just to chat!",
+    role: "Full-stack developer",
+    shortBio:
+      "Passionate about mastering web development across the stack. Tech addict. Syrup enthusiast. ",
+    socialMedia: {
+      Portfolio: "https://www.stackedpancakehouse.ca/breakfast.php",
+      Twitter: "https://twitter.com/pancakeswap",
+    },
+  },
+  {
+    userhandle: "troybarnes",
+    password: "123",
+    skills: ["HTML", "CSS", "Sass", "Photoshop"],
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.ae1qvbghge?alt=media&token=e4e4c32d-20a1-4c9e-8d4c-8674c112b11a",
+    bio: "I have an interest in making websites that have a more cinematic feel to it. I have experience in film as well, which greatly helps. Also check out my podcast: Troy and Abed in the Morning",
+    role: "UX/UI designer",
+    shortBio:
+      "I have a background in UI/UX design & would love to offer my services to others.",
+    socialMedia: {
+      Portfolio: "https://www.stackedpancakehouse.ca/breakfast.php",
+      GitHub: "http://google.com",
+      Twitter: "https://twitter.com/pancakeswap",
+    },
+  },
+  {
+    userhandle: "Chewbacca",
+    password: "123",
+    skills: ["HTML", "CSS", "Sass", "ReactJS"],
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.lusjl827ek?alt=media&token=564a692c-c25e-4b9f-823f-5e5ae7383110",
+    bio: "RRRAARRWHHGWWR. RRRAARRWHHGWWR. RRRAARRWHHGWWR. RRRAARRWHHGWWR.",
+    role: "Front-end developer",
+    shortBio: "RRWWWGG. RRRAARRWHHGWWR. RRRAARRWHHGWWR. ",
+    socialMedia: {
+      GitHub: "http://google.com",
+    },
+  },
+  {
+    userhandle: "sixseasons",
+    password: "123",
+    skills: ["NodeJS", "Express", "ReactJS", "AngularJS", "Git"],
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.w9gv0aqvft?alt=media&token=aa5ae677-4f64-41fe-ba67-473fad6f74d2",
+    bio: "I just want to work on other projects for fun. I already work full-time so only available for smaller projects.",
+    role: "Full-stack developer",
+    shortBio:
+      "Love all things tech and very experienced in terms of the user experience and the back-end dev't",
+    socialMedia: {
+      Portfolio: "http://google.com",
+      GitHub: "http://google.com",
+      LinkedIn: "http://google.com",
+      Twitter: "http://google.com",
+    },
+  },
+  {
+    userhandle: "Moe Szyslak",
+    password: "123",
+    skills: ["Figma", "Express", "NodeJS", "HTML", "CSS", "ReactJS", "Git"],
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.hsoktdkmcy?alt=media&token=82281a09-c59e-4351-9e93-5811b265a9be",
+    bio: "Moe, Moe, Moe! How do you like me? How do you like me? Moe, Moe, Moe! Why don't you like me? Nobody likes me...",
+    role: "Full-stack developer",
+    shortBio: "I own a bar. People come to my bar. That's it.",
+    socialMedia: {
+      GitHub: "http://google.com",
+    },
+  },
+  {
+    userhandle: "baconandeggs",
+    password: "123",
+    skills: ["Git", "NodeJS", "Express", "HTML", "CSS", "MongoDB", "SQL"],
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.9133alksly?alt=media&token=302d7039-8a42-4c61-90bb-8c9a090324a0",
+    bio: "Need to work with people who have a better eye for improving the user experience and have more experience with that kind of work.",
+    role: "Back-end developer",
+    shortBio:
+      "Previously worked on corporate websites and would love to work with someone more creative",
+    socialMedia: {
+      Portfolio: "http://google.com",
+      GitHub: "http://google.com",
+    },
+  },
+  {
+    userhandle: "Master Chief",
+    password: "123",
+    skills: ["HTML", "CSS", "Figma", "Photoshop", "UX research", "Git"],
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.9m88v7kq4o?alt=media&token=0404be94-b805-4155-971e-8a076c8da465",
+    bio: '"I do not remember my name. I do not remember my family. I do not remember my home. But I can remember the game. We played it every day. And I never lost. The game... It\'s the only thing I can remember about the life I had before I met Doctor Halsey. Since then, I have experienced a lifetime of combat. Through thirty years of war against alien aggressors... I have always known my fate. I knew someday I would die in battle. But now that it is here... now that it is time to die... I find I am not ready."',
+    role: "UX/UI designer",
+    shortBio:
+      'A towering supersoldier known as a "Spartan", raised and trained from childhood for combat.',
+    socialMedia: {
+      Twitter: "http://google.com",
+    },
+  },
+  {
+    userhandle: "NedQueens",
+    password: "123",
+    skills: ["ReactJS", "NodeJS", "MongoDB", "Express", "Git"],
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.hzkbzunfw4?alt=media&token=f7fdd3a0-8bcd-4024-b470-89627774618a",
+    bio: "I have some experience and am very tech-savy. I have worked on a couple projects myself but am not allowed to talk about it.",
+    role: "Full-stack developer",
+    shortBio:
+      "I'm just a guy from Queens who needs to practice more. So please DM me",
+    socialMedia: {
+      Twitter: "http://google.com",
+      GitHub: "http://google.com",
+    },
+  },
+  {
+    userhandle: "dwightschrute",
+    password: "123",
+    skills: ["HTML", "CSS", "NodeJS", "Express", "SQL", "AngularJS"],
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/collab-or-8.appspot.com/o/images%2F1.5f13pa4aej?alt=media&token=078dbb61-93fd-4fda-9013-fe889f99bdd7",
+    bio: "I have experience in the food industry which is why my interest expands into healthy eating on top of tech. I am a recent bootcamp graduate, I am practicing my skills and learning new technologies across the stack. I can sell anything, I just need someone to help me with the visual aspects of my proposals.",
+    role: "Full-stack developer",
+    shortBio:
+      "I am pretty new to web development and have a background in the corporate industry. ",
+    socialMedia: {
+      LinkedIn: "http://google.com",
+      GitHub: "http://google.com",
+    },
+  },
+];
 
 module.exports = seedUsers;
