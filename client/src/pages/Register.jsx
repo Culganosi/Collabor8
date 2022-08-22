0b3adc6fea50885c56556974c422ed574b92cc2e
Daria <daria.kiseleva@mail.mcgill.ca>
2022-08-22 09:11:29 -0400

    Edit package.json in client to fix esling error /?

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,56 +1,56 @@
 import "./Forms.css";
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from "react";
 import Button from "@material-ui/core/Button";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 // import Background from "./Background";
-import { Grid, Link } from "@material-ui/core"
+import { Grid, Link } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { DataContext } from "./../DataContext"
-import useStyles from '../styles';
-import { Container, Typography } from '@material-ui/core'
+import { DataContext } from "./../DataContext";
+import useStyles from "../styles";
+import { Container, Typography } from "@material-ui/core";
 import purplewave from "../media/purplewave.webp";
-import styled from "styled-components"
+import styled from "styled-components";
 
 export default function Register() {
+  //Scroll to top when entering page
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    //Scroll to top when entering page
-    useEffect(() => {
-      window.scrollTo(0, 0)
-    }, [])
-
-    
-    
   const classes = useStyles();
 
   const navigate = useNavigate();
   const { self, setSelf } = useContext(DataContext);
 
-
   const [userhandle, setUserhandle] = useState(undefined);
   const [password, setPassword] = useState(undefined);
 
-
   //FUNCTION TO REGISTER IN
   const register = () => {
-    axios.post("/api/auth/register", { userhandle, password })
-      .then(response => {
-        setSelf(response.data)
+    axios
+      .post("/api/auth/register", { userhandle, password })
+      .then((response) => {
+        setSelf(response.data);
       })
       .then(() => navigate("/Create-Profile"))
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
-    <div className="background">
+      <div className="background">
         <div className={classes.headercontainer}>
           <Container max-Width="sm">
-            <Typography variant="h2" align="center" color="secondary" gutterBottom>
+            <Typography
+              variant="h2"
+              align="center"
+              color="secondary"
+              gutterBottom
+            >
               Register
             </Typography>
-
           </Container>
         </div>
         <Box>
@@ -67,10 +67,10 @@ export default function Register() {
                   maxRows={1}
                   value={userhandle}
                   InputLabelProps={{
-                    style: { color: '#CF9FFF' },
+                    style: { color: "#CF9FFF" },
                   }}
                   InputProps={{
-                    style: {color: "#ffffff"}
+                    style: { color: "#ffffff" },
                   }}
                   onChange={(event) => setUserhandle(event.target.value)}
                 />
@@ -83,10 +83,10 @@ export default function Register() {
                   label="Password"
                   helperText="Please enter your password"
                   InputLabelProps={{
-                    style: { color: '#CF9FFF' },
+                    style: { color: "#CF9FFF" },
                   }}
                   InputProps={{
-                    style: {color: "#ffffff"}
+                    style: { color: "#ffffff" },
                   }}
                   multiline
                   maxRows={1}
@@ -101,12 +101,12 @@ export default function Register() {
                 color="secondary"
                 onClick={() => register()}
               >
-                Submit</Button>
-
+                Submit
+              </Button>
             </form>
           </div>
         </Box>
-        </div>    
-        </>
+      </div>
+    </>
   );
 }
